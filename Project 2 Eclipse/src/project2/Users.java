714b972462d1cecee2832fad36ab18c8815a8d0a
Taylor Baer <baertt@hendrix.edu>
2017-02-24 09:51:17 -0600

    Add to Eclipse
    For the like 10th time :-)

---
+++ Project 2 Eclipse/src/project2/Users.java
@@ -0,0 +1,13 @@
+package project2;
+
+import java.time.LocalDate;
+import java.util.Hashtable;
+
+public class Users {
+	private Hashtable<String, UserInfo> users = new Hashtable<String, UserInfo>();
+
+	public void add(String username, String password, String name, LocalDate bday,
+			        String phone, String email){
+		users.put(username, new UserInfo(password, name, bday, phone, email));
+	}
+}
